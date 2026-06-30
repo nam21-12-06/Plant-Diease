6006c5b675c6fd4e8facd084af3e104829aef870
Nam Nguyen <nam.nguyenthaihoai06@hcmut.edu.vn>
2026-06-30 15:16:59 +0700

    Add images size for predictor.py

--- app/predictor.py
+++ app/predictor.py
@@ -1,7 +1,11 @@
 import os
 import urllib.request
+
+import numpy as np
 import tensorflow as tf
 import streamlit as st
+
+IMG_SIZE = (256, 256)
 
 MODEL_URLS = {
     "models/baseline_cnn.keras": "https://github.com/nam21-12-06/Plant-Diease/releases/download/v1.0/baseline_cnn.keras",
